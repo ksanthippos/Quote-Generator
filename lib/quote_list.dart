2ff7ea6f409f50de06e6f4400ed8905722f3ab88
ksanthippos <mikael.rauhala@eduespoo.fi>
2021-04-28 11:34:47 +0300

    random scrollable quotes

--- lib/quote_list.dart
+++ lib/quote_list.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:quotes/quotes.dart';
 import 'package:flutter/material.dart';
 import 'quote.dart';
 import 'quote_card.dart';
+
+
 
 class QuoteList extends StatefulWidget {
   @override
@@ -10,11 +13,22 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<Quote> quotes = [
-    Quote(author: 'Chuck Norris', text: 'Chuck Norris can divide by zero.'),
-    Quote(author: 'Sun Tzu', text: 'Know your enemy.'),
-    Quote(author: 'Arnold Schwarzenegger', text: 'Milk is for babies. I drink BIER.'),
-  ];
+  List<Quote> quotes = [];
+
+  void getQuotes() {
+    for (int i = 0; i < 20; i++) {
+      quotes.add(Quote(
+        author: Quotes.getRandom().getAuthor(),
+        text: Quotes.getRandom().getContent()
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getQuotes();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +39,7 @@ class _QuoteListState extends State<QuoteList> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Column(
+      body: ListView(
         children: quotes.map((quote) => QuoteCard(
             quote: quote,
             delete: () {
